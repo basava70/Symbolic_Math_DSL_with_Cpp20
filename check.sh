cc1b7926e5f3d7cd3ac42bbdf52c417ba196f485
SeshuHomePc <seshu.basava70@gmail.com>
2025-04-21 20:18:18 +0200

    Changed CMakeLists, run.sh and check.sh

--- check.sh
+++ check.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
-cmake --build build
-./build/mathematical_expression_test
+
+if cmake --build build; then
+    echo "Build succeeded. Running tests..."
+    ./build/math_dsl_test
+else
+    echo "Build failed. Skipping tests."
+    exit 1
+fi
